7a8f0888a50027e4f0952b5d08939d950350ba26
KimADR <andriamahatoky.mael2020@gmail.com>
2024-12-25 19:52:11 +0300

    first commit in this project

---
+++ backend/index.js
@@ -0,0 +1,19 @@
+const express =  require('express');
+const mongoose = require('mongoose');
+const dotenv = require('dotenv');
+const app = express();
+const pinRoute = require('./routes/pins');
+
+dotenv.config();
+
+app.use(express.json());
+
+mongoose.connect(process.env.MONGO_URL, {useNewUrlParser: true}).then(() => {
+    console.log('Connected to MongoDB');
+} ).catch((err) => console.log(err));
+
+app.use('/api/pins', pinRoute);
+
+app.listen(8800, () => {
+    console.log('Server is running on port 8800');
+})
